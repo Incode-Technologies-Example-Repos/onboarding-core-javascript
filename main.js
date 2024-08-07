d47021620fbba684c49108007cc9005d7493c3e2
Levhita <armando.arias@incode.com>
2024-08-07 15:53:48 -0600

    updated error system to handle videoselfie

--- main.js
+++ main.js
@@ -2,36 +2,43 @@ import { fakeBackendStart, fakeBackendFinish } from './fake_backend'
 
 let incode;
 let incodeSession;
-const container = document.getElementById("camera-container");
+let showTutorialsFlag=false;
+const cameraContainer = document.getElementById("camera-container");
 
 function showError(e=null) {
-  container.innerHTML = "<h1>There was an error</h1>";
-  console.log(e.message)
+  const finishContainer = document.getElementById("finish-container");
+  if (e?.message) {
+    finishContainer.innerHTML = `<h1>Error: ${e.message}</h1>`;  
+  } else {
+    finishContainer.innerHTML = "<h1>There was an error</h1>";
+    console.log(e);
+  }
 }
 
 function saveDeviceData() {
   incode.sendGeolocation({ token: incodeSession.token });
   incode.sendFingerprint({ token: incodeSession.token });
+  //captureVideoSelfie();
   captureIdFrontSide();
 }
 
 function captureIdFrontSide() {
-  incode.renderCamera("front", container, {
+  incode.renderCamera("front", cameraContainer, {
     onSuccess: captureIdBackSide,
     onError: showError,
     token: incodeSession,
     numberOfTries: 3,
-    showTutorial: true
+    showTutorial: showTutorialsFlag
   });
 }
 
 function captureIdBackSide(response) {
-  incode.renderCamera("back", container, {
+  incode.renderCamera("back", cameraContainer, {
     onSuccess: processId,
     onError: showError,
     token: incodeSession,
     numberOfTries: 3,
-    showTutorial: true
+    showTutorial: showTutorialsFlag
   });
 }
 
@@ -44,42 +51,52 @@ async function  processId() {
 }
 
 function captureSelfie() {
-  incode.renderCamera("selfie", container, {
+  incode.renderCamera("selfie", cameraContainer, {
     onSuccess: captureVideoSelfie,
     onError: showError,
     token: incodeSession,
     numberOfTries: 3,
-    showTutorial: true
+    showTutorial: showTutorialsFlag
   });
 }
 
 function captureVideoSelfie(){
-  incode.renderVideoSelfie( container, {
-    token: incodeSession,
-    showTutorial: true,
-    modules: ["front", "back", "selfie", "speech"], // you can add 'poa' and 'questions'
-    speechToTextCheck: true, // this is the check for the speech
-  },
-  {
-    onSuccess: finishOnboarding,
-    onError: showError,
-    numberOfTries: 3,
+  incode.renderVideoSelfie( cameraContainer, {
+      token: incodeSession,
+      showTutorial: showTutorialsFlag,
+      modules: ["front", "back", "selfie", "speech"], // you can add 'poa' and 'questions'
+      speechToTextCheck: true, // this is the check for the speech
+      numberOfTries: 1,
+    },
+    {
+      onSuccess: finishOnboarding,
+      onError: showVideoSelfieError
+    }
+  );
+}
+
+function showVideoSelfieError(errors=[]) {
+  let error_string="<h1>There was some errors</h1>\n<ul>\n";
+  for (let i=0; i<errors.length;i++){
+    error_string +=`  <li>${errors[i]}</li>\n`
   }
-);
+  error_string+="</ul>"
+  const finishContainer = document.getElementById("finish-container");
+  finishContainer.innerHTML = error_string;
 }
 
 function finishOnboarding() {
   // Finishing the session works along with the configuration in the flow
   // webhooks and business rules are ran here.
   fakeBackendFinish(incodeSession.token)
-    .then((response) => {
-      console.log(response);
-      const container = document.getElementById("finish-container");
-      container.innerHTML = "<h1>Onboarding Finished.</h1>";
-    })
-    .catch((error) => {
-      showError(error);
-    });  
+  .then((response) => {
+    console.log(response);
+    const container = document.getElementById("finish-container");
+    container.innerHTML = "<h1>Onboarding Finished.</h1>";
+  })
+  .catch((e) => {
+    showError(e);
+  });  
 }
 
 async function app() {
@@ -90,21 +107,13 @@ async function app() {
     });
     
     // Create the single session
-    container.innerHTML = "<h1>Creating session...</h1>";
-    try {
-      incodeSession = await fakeBackendStart();
-    } catch(e) {
-      showError(e);
-      return;
-    }
-    
+    cameraContainer.innerHTML = "<h1>Creating session...</h1>";
+    incodeSession = await fakeBackendStart();
     // Empty the container and start the flow
-    container.innerHTML = "";
+    cameraContainer.innerHTML = "";
     saveDeviceData();
   } catch (e) {
-    console.dir(e);
-    container.innerHTML = "<h1>Something Went Wrong</h1>";
-    throw e;
+    showError(e);
   }
 }
 
